edc203222e827abb2b85bf905e4e0809de8ed716
Scott Fitzgerald <sfitzgerald@eero.com>
2019-08-24 14:09:27 -0500

    read song data into pg instead of using pandas

--- etl.py
+++ etl.py
@@ -18,44 +18,61 @@ def build_df(filepath):
     return df
 
 
-def get_column_names(cur, table_name):
-    """
-    returns a list of column names for a given table
-    :param table_name: the name of the table
-    :return <list>:  of column names
-    """
-    cur.execute(f"""
-        SELECT column_name
-        FROM information_schema.columns
-        WHERE table_name = '{table_name}';
-    """)
-    return [record[0] for record in cur.fetchall()]
+# def get_column_names(cur, table_name):
+#     """
+#     returns a list of column names for a given table
+#     :param table_name: the name of the table
+#     :return <list>:  of column names
+#     """
+#     cur.execute(f"""
+#         SELECT column_name
+#         FROM information_schema.columns
+#         WHERE table_name = '{table_name}';
+#     """)
+#     return [record[0] for record in cur.fetchall()]
+#
+#
+# def extract_table_data(cur, filepath, table_name):
+#     """
+#     Builds a dataframe and extracts desired data based on a given table's columns
+#     :param filename: the path to the filename to be read into the dataframe
+#     :param table_name: name of the table
+#     :return <list>: column values from dataframe corresponding to target table
+#     """
+#     df = build_df(filepath)
+#     table_cols = get_column_names(cur, table_name)
+#     col_numbers = {col: index for index, col in enumerate(df.columns, 0) if col in table_cols}
+#     return [df.values[0][col_numbers[name]] for name in table_cols], df
+#
+#
+def process_song_data(cur, filepath):
+    """Loads json song data into staging table"""
+    cur.execute(load_song_data_stage % filepath)
 
 
-def extract_table_data(cur, filepath, table_name):
-    """
-    Builds a dataframe and extracts desired data based on a given table's columns
-    :param filename: the path to the filename to be read into the dataframe
-    :param table_name: name of the table
-    :return <list>: column values from dataframe corresponding to target table
-    """
-    df = build_df(filepath)
-    table_cols = get_column_names(cur, table_name)
-    col_numbers = {col: index for index, col in enumerate(df.columns, 0) if col in table_cols}
-    return [df.values[0][col_numbers[name]] for name in table_cols], df
+def load_song_data(cur, conn):
+    """Loads data from staging table into songs and artists tables"""
+    cur.execute(songs_load)
+    cur.execute(artists_load)
+    conn.commit()
 
 
-def process_song_file(cur, filepath):
-    # open song file
-    df = pd.read_json(filepath, lines=True)
-
-    # insert song record
-    song_data, song_df = extract_table_data(cur, filepath, 'songs')
-    cur.execute(song_table_insert, song_data)
-
-    # insert artist record
-    artist_data, artist_df = extract_table_data(cur, filepath, 'artists')
-    cur.execute(artist_table_insert, artist_data)
+# def process_song_file(cur, filepath):
+#     # load file into staging table
+#     load_song_data_stage(filepath)
+#
+#     # insert song record
+#     # cur.execute(song_table_insert, song_data)
+#     # open song file
+#     # df = pd.read_json(filepath, lines=True)
+#     #
+#     # # insert song record
+#     # song_data, song_df = extract_table_data(cur, filepath, 'songs')
+#     # cur.execute(song_table_insert, song_data)
+#     #
+#     # # insert artist record
+#     # artist_data, artist_df = extract_table_data(cur, filepath, 'artists')
+#     # cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
@@ -100,14 +117,20 @@ def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
-    #
+
+    # # iterate over files and process
+    # for i, datafile in enumerate(all_files, 1):
+    #     func(cur, datafile)
+    #     conn.commit()
+    #     print('{}/{} files processed.'.format(i, num_files))
+
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
@@ -120,11 +143,15 @@ def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/song_data', func=process_song_data)
+    load_song_data(cur, conn)
+    # process_data(cur, conn, filepath='data/song_data', func=process_song_data)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
     conn.close()
 
-
+import time
 if __name__ == "__main__":
+    st = time.time()
     main()
+    print(f'runtime: {time.time() - st}')
