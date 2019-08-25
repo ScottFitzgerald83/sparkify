b53aaff5036d9b0d81ae7cf42b667d98c710eb4a
Scott Fitzgerald <sfitzgerald@eero.com>
2019-08-25 00:55:56 -0500

    refactor complete: move tbl load logic into one func

--- etl.py
+++ etl.py
@@ -1,37 +1,53 @@
 import os
 import glob
 import psycopg2
-import time
 
 import create_tables
 from sql_queries import *
 
 
 def stage_song_data(cur, filepath):
-    """Loads json song data into staging table"""
+    """
+    Loads json song data into staging table
+    :param cur: cursor from the psycopg2 connection object
+    :param filepath: path to song data json file
+    :return: None
+    """
     cur.execute(load_song_data_stage % filepath)
 
 
-def load_song_data(cur):
-    """Loads data from staging table into songs and artists tables"""
+def stage_log_data(cur, filepath):
+    """
+    Loads json log data into staging table
+    :param cur: cursor from the psycopg2 connection object
+    :param filepath: path to log data json file
+    :return: None
+    """
+    cur.execute(load_log_data_stage % filepath)
+    cur.execute(filter_log_data_stage)
+
+
+def load_tables(cur):
+    """
+    Executes SQL queries that load data from staging into prod tables
+    :param cur: cursor from the psycopg2 connection object
+    :return: None
+    """
     cur.execute(songs_load)
     cur.execute(artists_load)
-
-
-def stage_log_data(cur, filepath):
-    """Loads json log data into staging table"""
-    cur.execute(load_log_data_stage % filepath)
-
-
-def load_log_data(cur):
-    """Loads json log data into staging table"""
-    cur.execute(filter_log_data_stage)
     cur.execute(time_load)
     cur.execute(users_load)
     cur.execute(songplays_load)
 
 
 def process_data(cur, filepath, func):
+    """
+    Lists files in a given directory then passes log and song files to their respective processing function
+    :param cur: cursor from the psycopg2 connection object
+    :param filepath: the directory where the source data files live
+    :param func: the function which should operate on each file in the given directory
+    :return: None
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -56,15 +72,11 @@ def main():
     cur = conn.cursor()
 
     process_data(cur, filepath='data/song_data', func=stage_song_data)
-    load_song_data(cur)
-
     process_data(cur, filepath='data/log_data', func=stage_log_data)
-    load_log_data(cur)
+    load_tables(cur)
 
     conn.close()
 
 
 if __name__ == "__main__":
-    st = time.time()
     main()
-    print(f'runtime: {time.time() - st}')
